d2332c83a7c13124a141239b08408520db08bb66
Kazuki Tsujimura <63531246+kazuki0122@users.noreply.github.com>
2021-08-29 14:36:33 +0900

    ヘッダーの内容変更

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,5 @@
 import { AuthContext } from "App"
+import Group from "components/pages/Group"
 import React, { useContext } from "react"
 import { Route, Switch, Redirect } from "react-router-dom"
 import Home from "../components/pages/Home"
@@ -9,17 +10,13 @@ const Router: React.VFC = () => {
   const { loading, isSignedIn } = useContext(AuthContext)
   // ユーザーが認証済みかどうかでルーティングを決定
   // 未認証だった場合は「/login」ページに促す
-  const Private = ({ children }: { children: React.ReactElement }) => {
-    if (!loading) {
-      if (isSignedIn) {
-        return children
-      } else {
-        return <Redirect to="/login" />
-      }
-    } else {
-      return <></>
-    }
-  }
+  // const Private = ({ children }: { children: React.ReactElement }) => {
+  //     if (isSignedIn) {
+  //       return children
+  //     } else {
+  //       return <Redirect to="/login" />
+  //     }
+  //   }
 
   return (
     <Switch>
@@ -29,11 +26,12 @@ const Router: React.VFC = () => {
       <Route path='/signup'>
         <Signup />
       </Route>
-      <Private>
         <Route exact path="/">
           <Home />
         </Route>
-      </Private>
+        <Route path="/group/:id">
+          <Group />
+        </Route>
     </Switch>
   )
 }
